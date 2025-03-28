89702865ecbbb7685fcdc8026318c8f439eaa79b
Никита <147878497+AkinoF@users.noreply.github.com>
2025-03-28 10:57:42 +0300

    Update House.java
    убрал импорт VBox

--- src/House.java
+++ src/House.java
@@ -3,7 +3,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
